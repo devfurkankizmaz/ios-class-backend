3028b01205a19422bee6617fb9eca7b7eac6a998
devfurkankizmaz <dev.furkan@outlook.com>
2023-08-17 11:14:38 +0300

    Address location feautre

--- api/handlers/address.go
+++ api/handlers/address.go
@@ -37,6 +37,8 @@ func (ah *AddressHandler) Create(c echo.Context) error {
 		City:         payload.City,
 		Country:      payload.Country,
 		Address:      payload.Address,
+		Latitude:     payload.Latitude,
+		Longitude:    payload.Longitude,
 	}
 
 	err = ah.AddressService.Create(&newAddress)
@@ -65,6 +67,8 @@ func (ah *AddressHandler) FetchAllByUserID(c echo.Context) error {
 			City:         v.City,
 			Country:      v.Country,
 			Address:      v.Address,
+			Latitude:     v.Latitude,
+			Longitude:    v.Longitude,
 			CreatedAt:    v.CreatedAt,
 			UpdatedAt:    v.UpdatedAt,
 		}
@@ -89,6 +93,8 @@ func (ah *AddressHandler) FetchByID(c echo.Context) error {
 		City:         address.City,
 		Country:      address.Country,
 		Address:      address.Address,
+		Latitude:     address.Latitude,
+		Longitude:    address.Longitude,
 		CreatedAt:    address.CreatedAt,
 		UpdatedAt:    address.UpdatedAt,
 	}
@@ -118,6 +124,8 @@ func (ah *AddressHandler) UpdateByID(c echo.Context) error {
 		City:         payload.City,
 		Country:      payload.Country,
 		Address:      payload.Address,
+		Latitude:     payload.Latitude,
+		Longitude:    payload.Longitude,
 		UpdatedAt:    &cr,
 	}
 
