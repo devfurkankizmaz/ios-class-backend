ee9a9ae68669757c53f824ea93338881566156d0
devfurkankizmaz <dev.furkan@outlook.com>
2023-08-30 21:13:55 +0300

    register updated

--- models/register.go
+++ models/register.go
@@ -4,7 +4,6 @@ type RegisterInput struct {
 	FullName string `json:"full_name" validate:"required,min=4,max=30"`
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=6,max=25"`
-	PPUrl    string `json:"pp_url"`
 }
 
 type RegisterService interface {
